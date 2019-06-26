7962c26e11579edbea345759b55ce593197d7146
Esentur <esenturdildebekov8@gmail.com>
2019-06-27 01:21:56 +0600

    Выполнил домашнее задание №3. Прошу проверить

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,49 +1,60 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    static String[] people = {"Esentur", "Askar", "Beknur"};
-    static int selectedNumber;
 
     public static void main(String[] args) {
-        System.out.println("Основное задание.");
-        System.out.println("У нас есть "+people.length+ " человека");
-        for (int i = 0; i < people.length; i++) {
-            System.out.println(people[i] + " под индексом " + i);
-        }
-        wishes();
-        /*System.out.println("Задание на сообразительность");
-        String[] people = new String[4];
-        String[] people = {"Esentur", "Askar", "Beknur", "Andrey"};
-        wishes();*/
-    }
-
-    public static void wishes() {
+        //Основное задание
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Выберите индекс человека, которому хотите пожелать доброго времени суток.");
-        selectedNumber = in.nextInt();
+            String[] people = {"Esentur", "Askar", "Beknur"};
+            System.out.println("У нас есть " + people.length + " человека");
+            for (int i = 0; i < people.length; i++) {
+                System.out.println(people[i] + " под индексом " + i);
+            }
+            System.out.println("Выберите индекс человека, которому хотите пожелать доброго времени суток.");
+            int selectedNumber = in.nextInt();
+            wishes(people, selectedNumber);
+        //Задание на сообразительность
+
+            String[] people1 = Arrays.copyOf(people, 4); //переопределение массива типа String осуш-ся
+            people1[3] = "Andrey";                                  //путем копирования ее содрежимого + увел. размера
+            System.out.println("У нас есть " + people1.length + " человека");
+            for (int i = 0; i < people1.length; i++) {
+                System.out.println(people1[i] + " под индексом " + i);
+            }
+            System.out.println("Выберите индекс человека, которому хотите пожелать доброго времени суток.");
+            int selectedNumber1 = in.nextInt();
+            wishes(people1, selectedNumber1);
         in.close();
-        switch (selectedNumber) {
+    }
+
+    public static void wishes(String[] arr, int num) {
+
+        switch (num) {
             case 0:
-                System.out.println(people[0] + "! Доброе утро!");
+                System.out.println(arr[0] + "! Доброе утро!");
                 break;
             case 1:
-                System.out.println(people[1] + "! Добрый день!");
+                System.out.println(arr[1] + "! Добрый день!");
                 break;
             case 2:
-                System.out.println(people[2] + "! Добрый вечер!");
+                System.out.println(arr[2] + "! Добрый вечер!");
                 break;
             case 3:
-                if (people.length < 4) {
+                if (arr.length < 4) {
                     System.out.println("Вы не можете выбрать это число");
                 } else {
-                    System.out.println(people[3] + "! Доброй ночи!");
+                    System.out.println(arr[3] + "! Доброй ночи!");
                 }
                 break;
             default:
                 System.out.println("Нету человека под таким индексом");
+                break;
         }
+
     }
 }
+
